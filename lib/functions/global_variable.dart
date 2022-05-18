d03fab89c0417f11ca5160a7db700c9ba9d6b6d8
Dominique Youness <youness.dominique@gmail.com>
2022-05-18 11:40:36 +0200

    navbar and HomePage ok

--- lib/functions/global_variable.dart
+++ lib/functions/global_variable.dart
@@ -3,3 +3,11 @@ import 'package:flutter/material.dart';
 const Color primaryColor = Color(0xff10b981);
 const Color secondaryColor = Color(0xff9dada8);
 const Color tertColor = Color(0xffd7dbda);
+
+double height (context){
+  return MediaQuery.of(context).size.height;
+}
+
+double width(context){
+  return MediaQuery.of(context).size.width;
+}
