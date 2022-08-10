e3c475b33d4ac1637d5fa98b2dfba29157357c89
Dominique Youness <youness.dominique@gmail.com>
2022-08-10 10:50:56 +0200

    [FEAT] Login and Setup User Model

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,22 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart' as Dio;
+import '../models/user.dart';
 import 'dio.dart';
 
 class Auth extends ChangeNotifier{
   bool _isLoggedIn = false;
+  User? _user;
+  String? _token;
 
   bool get authenticated => _isLoggedIn;
+  User get user => _user!;
 
   void login ({required Map creds}) async {
 
     try {
       Dio.Response response = await dio()!.post('/sanctum/token', data: creds);
-      _isLoggedIn = true;
-      print('${response.data.toString()}');
+      String token = response.data.toString();
 
-      notifyListeners();
+      this.tryToken(token: token);
     } catch (e){
         print(e);
+    }
+  }
+
+  void tryToken({required String token}) async {
+    if(token == null){
+      return;
+    } else {
+      try {
+        Dio.Response response = await dio()!.post('/user',
+          options: Dio.Options(
+            headers: {'Authorization' : 'Bearer $token'}
+          )
+        );
+        this._isLoggedIn = true;
+        this._user = User.fromJson(response.data);
+        notifyListeners();
+      } catch (e){
+
+      }
     }
   }
 
