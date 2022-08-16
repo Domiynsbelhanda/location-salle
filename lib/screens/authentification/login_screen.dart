0a1a807f5dedd6162c8eed3d5406e19e21f0c0bf
Dominique Youness <youness.dominique@gmail.com>
2022-08-16 15:27:16 +0200

    [STYLE] Login Page

--- lib/screens/authentification/login_screen.dart
+++ lib/screens/authentification/login_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:place_event/utils/constant.dart';
 import 'package:provider/provider.dart';
 
 import '../../services/auth.dart';
@@ -54,46 +55,168 @@ class _LoginScreen extends State<LoginScreen>{
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Form(
-              key: _formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 60.0, 16.0,16.0),
+              child: Image.asset(
+                'assets/images/logo2.png',
+                width: MediaQuery.of(context).size.width / 1.5,
+                fit: BoxFit.contain,
+              ),
+            ),
+
+            Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 16.0, 0,0),
+                child: Text(
+                  'Connectez-vous',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 22.0
+                  ),
+                ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0,16.0),
+              child: Text(
+                'Connectez-vous en utilisant votre adresse mail et mot de passe',
+                style: TextStyle(
+                    color: secondaryColor,
+                    fontSize: 15.0
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Adresse mail',
+                      style: TextStyle(
+                          fontSize: 18.0
+                      ),
+                    ),
+                    SizedBox(height: 8.0),
+                    TextFormField(
+                      controller: _emailController,
+                      validator: (value) => value!.isEmpty ? 'Veuillez entrer un email valide' : null,
+                      decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          filled: true,
+                          hintStyle: TextStyle(color: Colors.grey[800]),
+                          hintText: "Enter your email adress",
+                          fillColor: Colors.white70
+                      ),
+                    ),
+
+                    SizedBox(height: 16.0),
+
+                    Text(
+                      'Mot de passe',
+                      style: TextStyle(
+                          fontSize: 18.0
+                      ),
+                    ),
+                    SizedBox(height: 8.0),
+                    TextFormField(
+                      controller: _passwordController,
+                      validator: (value) => value!.isEmpty ? 'Veuillez entrer un mot de passe valide' : null,
+                      decoration: InputDecoration(
+                          contentPadding: const EdgeInsets.symmetric(vertical: 5.0, horizontal: 10.0),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          filled: true,
+                          hintStyle: TextStyle(color: Colors.grey[800]),
+                          hintText: "Enter your password",
+                          fillColor: Colors.white70
+                      ),
+                    ),
+
+                    SizedBox(height: 16.0),
+
+                    SizedBox(
+                      width: double.infinity,
+                      height: 45.0,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Map data = {
+                            'email': _emailController.text,
+                            'password' : _passwordController.text,
+                            'device_name' : _deviceName ?? 'unknown',
+                          };
+                          if(_formKey.currentState!.validate()){
+                            Provider.of<Auth>(context, listen: false).login(creds: data);
+                            Navigator.pop(context);
+                          }
+                        },
+                        style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(primaryColor),
+                        ),
+                        child: Text(
+                          'CONNEXION'
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0,16.0),
+              child: Row(
                 children: [
-                  TextFormField(
-                    controller: _emailController,
-                    validator: (value) => value!.isEmpty ? 'Veuillez entrer un email valide' : null,
+                  Text(
+                    'Vous n\'avez pas de compte? ',
+                    style: TextStyle(
+                        fontSize: 15.0
+                    ),
                   ),
+                  GestureDetector(
+                    onTap: (){
 
-                  TextFormField(
-                    controller: _passwordController,
-                    validator: (value) => value!.isEmpty ? 'Veuillez entrer un mot de passe valide' : null,
-                  ),
-
-                  ElevatedButton(
-                    onPressed: () {
-                      Map data = {
-                        'email': _emailController.text,
-                        'password' : _passwordController.text,
-                        'device_name' : _deviceName ?? 'unknown',
-                      };
-                      if(_formKey.currentState!.validate()){
-                        Provider.of<Auth>(context, listen: false).login(creds: data);
-                        Navigator.pop(context);
-                      }
                     },
                     child: Text(
-                      'CONNEXION'
+                      'Cliquez ici.',
+                      style: TextStyle(
+                        color: primaryColor,
+                          fontSize: 15.0
+                      ),
                     ),
                   )
                 ],
               ),
             ),
-          )
-        ],
+
+            Padding(
+              padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0,16.0),
+              child: GestureDetector(
+                onTap: (){
+                  Navigator.pop(context);
+                },
+                child: Text(
+                  'Fermer la page d\'authentification.',
+                  style: TextStyle(
+                      color: secondaryColor,
+                      fontSize: 15.0
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
