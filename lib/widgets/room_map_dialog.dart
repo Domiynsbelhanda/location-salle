00b8a912726cb4af83ba8bccc780b35196181e12
Dominique Youness <youness.dominique@gmail.com>
2022-08-24 03:21:41 +0200

    [STYLE] Dialog custom featured

--- lib/widgets/room_map_dialog.dart
+++ lib/widgets/room_map_dialog.dart
@@ -1,21 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:place_event/models/rooms.dart';
 
-show_room_map_dialog(BuildContext context, String titre, String message) {
+import '../screens/details.dart';
+import 'feature_item.dart';
+
+show_room_map_dialog(BuildContext context, Rooms rooms) {
 
   // set up the button
   Widget okButton = TextButton(
-    child: Text("OK"),
+    child: Text("FERMER"),
     onPressed: () {
       Navigator.pop(context);
     },
   );
 
+  Widget plusButton = TextButton(
+    child: Text("VOIR PLUS"),
+    onPressed: () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => Details(
+          data: rooms,
+        )),
+      );
+    },
+  );
+
   // set up the AlertDialog
   AlertDialog alert = AlertDialog(
-    title: Text('$titre'),
-    content: Text('$message.'),
+    title: Text('${rooms.title}'),
+    content: FeatureItem(
+      data: rooms,
+      onTapFavorite: () {
+      },
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Details(
+            data: rooms,
+          )),
+        );
+      },
+    ),
     actions: [
-      okButton,
+      plusButton,
+      okButton
     ],
   );
 
